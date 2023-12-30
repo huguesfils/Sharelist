856388c72477b20b2df2a769eb657bbaf5829a40
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2023-12-30 17:17:57 +0100

    warning fix

--- Sharelist/Core/Model/AnalyticsManager.swift
+++ Sharelist/Core/Model/AnalyticsManager.swift
@@ -5,17 +5,17 @@
 //  Created by Hugues Fils on 30/12/2023.
 //
 
-import Foundation
-import FirebaseAnalytics
-
-final class AnalyticsManager {
-    
-    private init() {}
-    
-    static let shared = AnalyticsManager()
-    
-    public func logEventAccountCreated() {
-//        Analytics.logEvent("AccountCreated", parameters: <#T##[String : Any]?#>)
-    }
-    
-}
+//import Foundation
+//import FirebaseAnalytics
+//
+//final class AnalyticsManager {
+//    
+//    private init() {}
+//    
+//    static let shared = AnalyticsManager()
+//    
+//    public func logEventAccountCreated() {
+//
+//    }
+//    
+//}
